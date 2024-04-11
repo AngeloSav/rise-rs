2e9100cd49e12a871746abcb01f04cc393b94b28
Rossano Venturini <rossano.venturini@gmail.com>
2024-04-11 02:46:55 +0000

    Implement concat

--- pef/src/bitvector/bitvector_collection.rs
+++ pef/src/bitvector/bitvector_collection.rs
@@ -39,11 +39,19 @@ impl BitVecCollection {
     pub fn get(&self, i: usize) -> BitSliceWithOffset {
         assert!(i < self.n_vecs, "Index out of bounds");
 
-        let start = self.endpoints[i];
-        let end = self.endpoints[i + 1];
+        let start_bit = self.endpoints[i];
+        let end_bit = self.endpoints[i + 1];
+        let n_bits = end_bit - start_bit;
 
-        let n_bits = end - start;
-        unsafe { BitSliceWithOffset::from_raw_parts(&self.bv.data[start..end], n_bits, start % 64) }
+        let start_word = start_bit / 64;
+        let end_word = (end_bit + 63) / 64;
+        let offset = start_bit % 64;
+
+        dbg!(start_word, end_word, offset, n_bits, start_bit, end_bit);
+
+        unsafe {
+            BitSliceWithOffset::from_raw_parts(&self.bv.data[start_word..end_word], n_bits, offset)
+        }
     }
 
     pub fn len(&self) -> usize {
@@ -134,10 +142,83 @@ impl<'a> BitSliceWithOffset<'a> {
         BitVector::<&[u64]>::get_bits_slice(self.data.as_ref(), index + self.offset, len)
     }
 
+    /// Returns a non-consuming iterator over positions of bits set to 1 in the bit vector.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use pef::BitVec;
+    ///
+    /// let vv: Vec<usize> = vec![0, 63, 128, 129, 254, 1026];
+    /// let bv: BitVec = vv.iter().copied().collect();
+    ///
+    /// let v: Vec<usize> = bv.ones().collect();
+    /// assert_eq!(v, vv);
+    /// ```
+    #[must_use]
+    pub fn ones(&self) -> BitVectorBitPositionsIter<true> {
+        BitVectorBitPositionsIter::with_pos(
+            self.data.as_ref(),
+            self.n_bits + self.offset,
+            self.offset,
+        )
+    }
+
+    /// Returns a non-consuming iterator over positions of bits set to 1 in the bit vector, starting at a specified bit position.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use pef::BitVec;
+    ///
+    /// let vv: Vec<usize> = vec![0, 63, 128, 129, 254, 1026];
+    /// let bv: BitVec = vv.iter().copied().collect();
+    ///
+    /// let v: Vec<usize> = bv.ones_with_pos(2).collect();
+    /// assert_eq!(v, vec![63, 128, 129, 254, 1026]);
+    /// ```
+    #[must_use]
+    pub fn ones_with_pos(&self, pos: usize) -> BitVectorBitPositionsIter<true> {
+        BitVectorBitPositionsIter::with_pos(
+            self.data.as_ref(),
+            self.n_bits + self.offset,
+            self.offset + pos,
+        )
+    }
+
+    /// Returns a non-consuming iterator over positions of bits set to 0 in the bit vector.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use pef::BitVec;
+    /// use pef::gen_sequence::negate_vector;
+    ///
+    /// let vv: Vec<usize> = vec![0, 63, 128, 129, 254, 1026];
+    /// let bv: BitVec = vv.iter().copied().collect();
+    ///
+    /// let v: Vec<usize> = bv.zeros().collect();
+    /// assert_eq!(v, negate_vector(&vv));
+    /// ```
+    #[must_use]
+    pub fn zeros(&self) -> BitVectorBitPositionsIter<false> {
+        BitVectorBitPositionsIter::with_pos(self.data.as_ref(), self.n_bits, self.offset)
+    }
+
+    /// Returns a non-consuming iterator over positions of bits set to 0 in the bit vector, starting at a specified bit position.
+    #[must_use]
+    pub fn zeros_with_pos(&self, pos: usize) -> BitVectorBitPositionsIter<false> {
+        BitVectorBitPositionsIter::with_pos(self.data.as_ref(), self.n_bits, self.offset + pos)
+    }
+
+    #[inline]
+    #[must_use]
     pub fn len(&self) -> usize {
         self.n_bits
     }
 
+    #[inline]
+    #[must_use]
     pub fn is_empty(&self) -> bool {
         self.len() == 0
     }
@@ -147,7 +228,9 @@ impl AccessBin for BitSliceWithOffset<'_> {
     #[inline]
     #[must_use]
     fn get(&self, index: usize) -> Option<bool> {
-        if index < self.n_bits {
+        dbg!(index, self.n_bits, self.offset);
+
+        if index >= self.n_bits {
             return None;
         }
         Some(unsafe { self.get_unchecked(index) })
@@ -156,5 +239,52 @@ impl AccessBin for BitSliceWithOffset<'_> {
     unsafe fn get_unchecked(&self, index: usize) -> bool {
         debug_assert!(index < self.n_bits, "Index out of bounds");
         BitVector::<&[u64]>::get_bit_slice(self.data, index + self.offset)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    //use crate::gen_sequence::gen_strictly_increasing_sequence;
+
+    #[test]
+    fn test_bitvec_collection() {
+        let mut bvc = BitVecCollection::default();
+        assert!(bvc.is_empty());
+
+        let vv1: Vec<usize> = vec![0, 63, 128, 129, 254, 1026];
+        let bv: BitVec = vv1.iter().copied().collect();
+        bvc.push(&bv);
+
+        assert_eq!(bvc.len(), 1);
+        assert!(!bvc.is_empty());
+
+        let bv = BitVec::default();
+        bvc.push(&bv);
+        assert_eq!(bvc.len(), 2);
+
+        let vv2: Vec<usize> = vec![0, 61, 127, 130, 242, 365];
+        let bv: BitVec = vv2.iter().copied().collect();
+        bvc.push(&bv);
+        assert_eq!(bvc.len(), 3);
+
+        let bswo = bvc.get(0);
+        assert_eq!(bswo.len(), 1027);
+        assert_eq!(bswo.get(0), Some(true));
+        assert_eq!(bswo.get(63), Some(true));
+        assert_eq!(bswo.get(64), Some(false));
+        assert_eq!(bswo.get(1026), Some(true));
+
+        assert_eq!(bswo.ones().collect::<Vec<usize>>(), vv1);
+
+        let bswo = bvc.get(1);
+        assert_eq!(bswo.len(), 0);
+        assert_eq!(bswo.get(0), None);
+        assert_eq!(bswo.ones().collect::<Vec<usize>>(), vec![]);
+
+        let bswo = bvc.get(2);
+        assert_eq!(bswo.len(), 366);
+
+        assert_eq!(bswo.ones().collect::<Vec<usize>>(), vv2);
     }
 }
